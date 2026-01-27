24ab046e98ba589a29926e2ad7937a029ae5a8db
Kaylin98 <kaylinmaharaj@gmail.com>
2026-01-27 18:59:27 +0200

    feat: enhance player movement clamping logic for smoother control

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 50f;
-
+    [SerializeField] float xClampRange = 30f;
+    [SerializeField] float yClampRange = 20f;
     Vector2 moveInput;
+
+
    
     private void Update()
     {
@@ -20,7 +23,13 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = moveInput.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
         float yOffset = moveInput.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
