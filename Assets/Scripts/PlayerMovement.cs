4f801155306bb0c43899228232afb9592af54abf
Kaylin98 <kaylinmaharaj@gmail.com>
2026-01-27 18:51:00 +0200

    feat: implement basic player movement logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public void OnMove(InputValue value)
+    [SerializeField] float controlSpeed = 50f;
+
+    Vector2 moveInput;
+   
+    private void Update()
     {
-        Debug.Log("Move input received: " + value.Get<Vector2>());
+        ProcessTranslation();
     }
 
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
     
+    private void ProcessTranslation()
+    {
+        float xOffset = moveInput.x * controlSpeed * Time.deltaTime;
+        float yOffset = moveInput.y * controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+    }
 }
