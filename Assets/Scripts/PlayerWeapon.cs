40dbe7124edcb71011ca75a81b69c7139cadb26e
Kaylin98 <kaylinmaharaj@gmail.com>
2026-01-29 18:06:14 +0200

    feat: implement custom crosshair UI

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,17 +4,24 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+
     bool isFiring = false;
 
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    
     void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
     }
     
     public void OnFire(InputValue value)
     {
         isFiring = value.isPressed;
-        Debug.Log("Fire button pressed!");
     }
 
     void ProcessFiring()
@@ -24,5 +31,10 @@ public class PlayerWeapon : MonoBehaviour
             var laserEmission = laser.GetComponent<ParticleSystem>().emission;
             laserEmission.enabled = isFiring;
         }
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Mouse.current.position.ReadValue();
     }
 }
