c6543c5bd7f745427e56453025840531c579d18d
Kaylin98 <kaylinmaharaj@gmail.com>
2026-02-08 18:35:44 +0200

    feat: add Scoreboard logic and connect to enemy destruction
    Created Scoreboard.cs to track points. Updated Enemy.cs to find the Scoreboard instance and award points on death. Added Score Board object to MainLevel.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,11 +3,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject explosionVFX;   
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+    
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
     
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(explosionVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        KillEnemy();
     }
+
+    private void KillEnemy()
+    {
+
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(explosionVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
+    }
+
 }
