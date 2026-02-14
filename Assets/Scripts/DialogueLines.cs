7b011b87d4e98462e85bf06c9d6dda8adec1b567
Kaylin98 <kaylinmaharaj@gmail.com>
2026-02-14 17:40:06 +0200

    feat: add voice overs and signal-based dialogue system

---
+++ Assets/Scripts/DialogueLines.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class DialogueLines : MonoBehaviour
+{
+   [SerializeField] TMP_Text dialogueText;
+   [SerializeField] string[] lines;
+   int currentLine = 0; 
+
+   public void NextDialogueLine()
+   {
+    if (currentLine < lines.Length)
+    {
+        currentLine++;
+        dialogueText.text = lines[currentLine];
+        
+    }
+    else
+    {
+        dialogueText.text = "";
+        currentLine = 0; 
+    }
+   }    
+}
