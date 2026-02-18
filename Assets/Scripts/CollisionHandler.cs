e10fc71163d2b91fc96efb689c9e1c9ec0ffc962
Kaylin98 <kaylinmaharaj@gmail.com>
2026-02-18 21:53:06 +0200

    feat: implement GameSceneManager for level reloading and remove unused LaunchConifersInstaller

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
      [SerializeField] GameObject explosionVFX;   
+
+    GameSceneManager gameSceneManager;
+
+    void Start()
+    {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         Instantiate(explosionVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
+        gameSceneManager.ReloadLevel();
     }
 }
