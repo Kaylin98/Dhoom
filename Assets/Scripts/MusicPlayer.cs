ab45512558cc55a517fb9d8188c098e718ead45a
Kaylin98 <kaylinmaharaj@gmail.com>
2026-02-20 21:51:40 +0200

    feat: add radio-style VO and implement music ducking via Audio Mixer

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,21 +2,28 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // This creates a permanent memory of the very first MusicPlayer
+    private static MusicPlayer instance;
+
+    // Awake happens BEFORE Start, and BEFORE "Play On Awake" audio triggers
+    void Awake()
     {
-        int numOfMusicPlayers = FindObjectsByType<MusicPlayer>(FindObjectsSortMode.None).Length;
-
-        if (numOfMusicPlayers > 1)
+        // 1. If an instance already exists, and it's not THIS one...
+        if (instance != null && instance != this)
         {
+            // Instantly destroy this duplicate before it can play a single frame of music
             Destroy(gameObject);
+            return;
         }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-        
-    }
 
-   
+        // 2. If this is the very first one, claim the "instance" title
+        instance = this;
+
+        // 3. Force this object to be a Root object. 
+        // (If it has a parent, DontDestroyOnLoad will fail and the music will restart!)
+        transform.SetParent(null); 
+        
+        // 4. Survive scene reloads
+        DontDestroyOnLoad(gameObject);
+    }
 }
