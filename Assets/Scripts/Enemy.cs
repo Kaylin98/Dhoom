286672b9a2436e3cc35f3464a5c59de1c938bef9
Kaylin98 <kaylinmaharaj@gmail.com>
2026-03-07 21:41:19 +0200

    feat: Add new playable timeline for Three Scarabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionVFX;
+    [SerializeField] AudioClip hitSound;
     [SerializeField] int hitPoints = 3;
     [SerializeField] int scoreValue = 10;
     
     Scoreboard scoreboard;
+    AudioSource audioSource; 
 
     private void Start()
     {
         scoreboard = FindFirstObjectByType<Scoreboard>();
+        audioSource = GetComponent<AudioSource>();
     }
     
     void OnParticleCollision(GameObject other)
     {
-        KillEnemy();
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        audioSource.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+        audioSource.PlayOneShot(hitSound);
+
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
     }
 
     private void KillEnemy()
     {
-        hitPoints--;
-        if (hitPoints <= 0)
-        {
-            scoreboard.IncreaseScore(scoreValue);
-            
-            GameObject vfx = Instantiate(explosionVFX, transform.position, transform.rotation);
-            
-            Destroy(vfx, 2f); 
-            Destroy(gameObject);
-        }
+        scoreboard.IncreaseScore(scoreValue);
         
+        GameObject vfx = Instantiate(explosionVFX, transform.position, transform.rotation);
+        
+        Destroy(vfx, 2f); 
+        Destroy(gameObject); // Enemy is destroyed here
     }
-
 }
